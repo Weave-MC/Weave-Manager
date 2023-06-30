e5ea241d055ca8907d40b37cf3fb071f364a610b
exejar <56853437+exejar@users.noreply.github.com>
2023-06-30 16:31:52 -0400

    Added basic setting toggles

---
+++ src/components/Settings.svelte
@@ -0,0 +1,31 @@
+<script>
+    import TogglePill from "./TogglePill.svelte";
+
+    function something(event) {
+        console.log(event.detail.active)
+    }
+</script>
+
+<div id="settings-content" class="w-full h-full flex flex-col">
+    <div id="settings-title" class="w-full h-8 flex justify-center items-center border-b-2 border-surface1">
+        <h1>Settings</h1>
+        <i class="absolute right-6 fa-solid fa-gear"></i>
+    </div>
+    <div id="settings" class="flex flex-col w-full h-full pt-2 pb-8 gap-2">
+        <div class="setting-toggle">
+            <TogglePill id="prompt-weave" name="Prompt Relaunch" on:toggle={something}/>
+        </div>
+        <div class="setting-toggle">
+            <TogglePill id="startup-run" name="Run on Startup"/>
+        </div>
+        <div class="setting-toggle">
+            <TogglePill id="auto-update" name="Auto Update"/>
+        </div>
+    </div>
+</div>
+
+<style>
+    .setting-toggle {
+        @apply w-full h-8 flex flex-row items-center pl-2;
+    }
+</style>
---
+++ src/components/TogglePill.svelte
@@ -0,0 +1,64 @@
+<script>
+    import { createEventDispatcher } from "svelte";
+
+    export let id;
+    export let name;
+    const dispatch = createEventDispatcher()
+
+    function onToggle(event) {
+        dispatch('toggle', {
+            active: event.target.checked
+        })
+    }
+</script>
+
+<div class="toggle-pill flex flex-row items-center">
+    <div class="checkbox-container mr-2">
+        <input type="checkbox" id="{id}" name="check" on:click={onToggle}>
+        <label for="{id}"></label>
+    </div>
+    <label class="cursor-pointer" for={id}>{name}</label>
+</div>
+
+<style>
+    input[type="checkbox"] {
+        display: none;
+    }
+    input[type="checkbox"] + label {
+        @apply bg-red;
+
+        display: block;
+        position: relative;
+        width: 2rem;
+        height: 1.25rem;
+        border-radius: 1rem;
+        box-shadow: inset 0 0 5px rgba(0, 0, 0, 0.3);
+        cursor: pointer;
+        -webkit-user-select: none;
+        -moz-user-select: none;
+        -ms-user-select: none;
+        transition: background 0.1s ease-in-out;
+    }
+    input[type="checkbox"] + label:before {
+        @apply bg-text;
+
+        content: "";
+        display: block;
+        width: 0.85rem;
+        height: 0.85rem;
+        border-radius: 1rem;
+        box-shadow: 2px 0 5px rgba(0, 0, 0, 0.2);
+        position: absolute;
+        left: 0.2rem;
+        top: 0.2rem;
+        -webkit-transition: all 0.2s ease-in-out;
+        transition: all 0.2s ease-in-out;
+    }
+    input[type="checkbox"]:checked + label {
+        @apply bg-teal;
+    }
+    input[type="checkbox"]:checked + label:before {
+        box-shadow: -2px 0 5px rgba(0, 0, 0, 0.2);
+        left: 0.95rem;
+    }
+</style>
