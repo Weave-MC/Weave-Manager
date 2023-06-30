c1f34f0c0ec25ad429e18ec8a49230eadf121c30
exejar <56853437+exejar@users.noreply.github.com>
2023-06-30 18:02:16 -0400

    Added proof of concept analytics panel

--- src/components/Analytics.svelte
+++ src/components/Analytics.svelte
@@ -1,3 +1,27 @@
-<div id="analytics" class="w-full h-full">
+<script>
+    import LineGraph from "./LineGraph.svelte";
+    import Statistic from "./Statistic.svelte";
+</script>
 
+<div id="analytics" class="w-full h-full">
+    <div id="analytics-title" class="w-full h-8 flex justify-center items-center border-b-2 border-surface1">
+        <h1 class="absolute">Analytics</h1>
+        <div class="w-full flex justify-end px-2">
+            <i class="fa-solid fa-chart-simple"></i>
+        </div>
+    </div>
+    <div id="content" class="w-full h-full pb-8">
+        <div id="statistics" class="w-full h-full flex flex-row flex-wrap items-center justify-center">
+            <Statistic name="Hours Played" value="10"/>
+            <Statistic name="Avg. Launch" value="32.3s"/>
+            <Statistic name="Instances" value="1"/>
+            <Statistic name="Mem Usage" value="5%"/>
+        </div>
+    </div>
 </div>
+
+<style>
+    .statistic {
+
+    }
+</style>
---
+++ src/components/Statistic.svelte
@@ -0,0 +1,9 @@
+<script lang="ts">
+    export let name
+    export let value
+</script>
+
+<div id="statistic" class="w-1/2 h-1/4 flex flex-col items-center justify-center">
+    <h1 class="font-semibold">{name}</h1>
+    <h1>{value}</h1>
+</div>
