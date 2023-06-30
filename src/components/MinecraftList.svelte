4d8233332268513646b0754d296d645044e66288
exejar <56853437+exejar@users.noreply.github.com>
2023-06-30 16:57:07 -0400

    Added titlebars

--- src/components/MinecraftList.svelte
+++ src/components/MinecraftList.svelte
@@ -1,3 +1,8 @@
 <div id="minecraft-list" class="w-full h-full">
-
+    <div id="minecraft-list-title" class="w-full h-8 flex justify-center items-center border-b-2 border-surface1">
+        <h1 class="absolute">Minecraft Processes</h1>
+        <div class="w-full flex justify-end px-2">
+            <i class="fa-solid fa-display"></i>
+        </div>
+    </div>
 </div>
