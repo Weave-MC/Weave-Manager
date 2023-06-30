70bde71f36628655a263c0cf465cfd36f722a3b6
exejar <56853437+exejar@users.noreply.github.com>
2023-06-30 10:13:59 -0400

    Added basic HeaderBar

--- src/App.svelte
+++ src/App.svelte
@@ -1,6 +1,8 @@
 <script lang="ts">
+import HeaderBar from "./components/HeaderBar.svelte";
 </script>
 
-<main>
+<main id="main" class="w-screen h-screen text-neutral-200">
+  <HeaderBar/>
   <h1 class="text-red-500">test</h1>
 </main>
---
+++ src/components/HeaderBar.svelte
@@ -0,0 +1,36 @@
+<div data-tauri-drag-region class="titlebar relative text-xl w-full bg-neutral-900 h-10 flex items-center pl-3 gap-3">
+    <div id="header-buttons" class="absolute right-0 h-10 flex justify-end gap-2 p-2">
+        <button class="button hover:bg-neutral-600" on:click={minimize}>
+            <i class="fa-solid fa-minus"></i>
+        </button>
+        <button class="button hover:bg-red-500 hover:bg-opacity-80" on:click={close}>
+            <i class="fa-solid fa-xmark"></i>
+        </button>
+    </div>
+    <img src="icon.png" class="w-7 h-7" alt="Weave Icon">
+    <h1>Weave Manager</h1>
+</div>
+
+<script lang="ts">
+    import { appWindow } from '@tauri-apps/api/window'
+
+    function minimize() {
+        appWindow.minimize()
+    }
+
+    function close() {
+        appWindow.minimize()
+    }
+</script>
+
+<style>
+    #header {
+        z-index: 1;
+    }
+
+    .button {
+        @apply h-7 aspect-square text-lg text-white transition-colors duration-300 rounded;
+        -webkit-app-region: no-drag;
+        text-shadow: -1px -1px 0 #00000030, 1px -1px 0 #00000030, -1px 1px 0 #00000030, 1px 1px 0 #00000030;
+    }
+</style>
