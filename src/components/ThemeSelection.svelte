064034c61ed70cb11394955cdf98a020f77e96af
exejar <56853437+exejar@users.noreply.github.com>
2023-07-01 19:40:24 -0400

    start of new themes

--- src/components/ThemeSelection.svelte
+++ src/components/ThemeSelection.svelte
@@ -1,16 +1,28 @@
 <div id="theme-selection" class="relative w-7 h-7 rounded-lg bg-base overflow-hidden">
     <div id="selections" class="absolute w-48 h-56 pt-7 flex flex-col justify-around">
-        <div id="mocha-theme" class="theme" on:click={() => selectTheme('mocha')}>
-            <h1>Mocha</h1>
+        <div id="darcula-theme" class="theme" on:click={() => selectTheme('mocha')}>
+            <h1>Darcula</h1>
         </div>
-        <div id="macchiato-theme" class="theme" on:click={() => selectTheme('macchiato')}>
-            <h1>Macchiato</h1>
+        <div id="light-theme" class="theme" on:click={() => selectTheme('macchiato')}>
+            <h1>Light</h1>
         </div>
-        <div id="latte-theme" class="theme" on:click={() => selectTheme('latte')}>
-            <h1>Latte</h1>
+        <div id="moonlight-theme" class="theme" on:click={() => selectTheme('latte')}>
+            <h1>Moonlight</h1>
         </div>
-        <div id="frappe-theme" class="theme" on:click={() => selectTheme('frappe')}>
-            <h1>Frappe</h1>
+        <div id="brimstone-theme" class="theme" on:click={() => selectTheme('frappe')}>
+            <h1>Brimstone</h1>
+        </div>
+        <div id="purple-rain-theme" class="theme" on:click={() => selectTheme('frappe')}>
+            <h1>Purple Rain</h1>
+        </div>
+        <div id="monokai-theme" class="theme" on:click={() => selectTheme('frappe')}>
+            <h1>Monokai</h1>
+        </div>
+        <div id="forest" class="theme" on:click={() => selectTheme('frappe')}>
+            <h1>Forest</h1>
+        </div>
+        <div id="oled-dark-theme" class="theme" on:click={() => selectTheme('frappe')}>
+            <h1>OLED Dark</h1>
         </div>
     </div>
     <div id="title" class="absolute w-48 h-7 pl-3 flex items-center">
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,20 @@
 export default {
   content: ["./src/**/*.svelte"],
   theme: {
-    extend: {},
+    colors: {
+      'crust': 'rgb(var(--color-crust)',
+      'base': 'rgb(var(--color-base)',
+      'surface0': 'rgb(var(--color-surface0)',
+      'surface1': 'rgb(var(--color-surface1)',
+      'overlay0': 'rgb(var(--color-overlay0)',
+      'overlay1': 'rgb(var(--color-overlay1)',
+      'text': 'rgb(var(--color-text)',
+      'red': 'rgb(var(--color-red)',
+      'green': 'rgb(var(--color-green)',
+      'pink': 'rgb(var(--color-pink)',
+      'blue': 'rgb(var(--color-blue)',
+      'orange': 'rgb(var(--color-orange)',
+    }
   },
   safelist: [
     {
