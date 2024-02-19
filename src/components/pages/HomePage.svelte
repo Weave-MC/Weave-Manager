75e297a1955ba046269de6a7275d5ce10ba4e69b
exejar <56853437+exejar@users.noreply.github.com>
2024-02-19 13:54:41 -0500

    Added glider for sub page selection

--- src/components/pages/HomePage.svelte
+++ src/components/pages/HomePage.svelte
@@ -4,13 +4,51 @@
     import ModsSubPage from "./sub/ModsSubPage.svelte";
 
     let selectedInnerPage = OverviewSubPage
+    let selectedClass = "overview"
+
+    function selectPage(page: any) {
+        selectedClass = page
+        switch (page) {
+            case "overview": {
+                selectedInnerPage = OverviewSubPage
+                break
+            }
+            case "game": {
+                selectedInnerPage = GameSubPage
+                break
+            }
+            case "mods": {
+                selectedInnerPage = ModsSubPage
+                break
+            }
+        }
+    }
 </script>
 
-<div id="inner-nav" class="w-full h-[10%] bg-surface rounded-lg flex flex-row justify-around items-center text-xl font-semibold">
-    <button on:click={() => selectedInnerPage = OverviewSubPage}>Overview</button>
-    <button on:click={() => selectedInnerPage = GameSubPage}>Game</button>
-    <button on:click={() => selectedInnerPage = ModsSubPage}>Mods</button>
+<div id="inner-nav" class="w-full h-[10%] bg-surface rounded-lg px-16">
+    <div id="content" class="relative w-full h-full flex flex-row justify-between items-center text-xl font-semibold">
+        <button class="w-1/5 h-full" on:click={() => selectPage("overview")}>Overview</button>
+        <button class="w-1/5 h-full" on:click={() => selectPage("game")}>Game</button>
+        <button class="w-1/5 h-full" on:click={() => selectPage("mods")}>Mods</button>
+
+        <span id="glider" class="absolute bottom-0 rounded-t-lg h-1.5 w-1/5 bg-accent {selectedClass}"/>
+    </div>
 </div>
 <div id="page" class="relative w-full h-[90%] rounded-lg">
     <svelte:component this={selectedInnerPage}/>
 </div>
+
+<style>
+    #glider {
+        transition: left 250ms;
+    }
+    .overview {
+        left: 0;
+    }
+    .game {
+        left: 40%;
+    }
+    .mods {
+        left: 80%;
+    }
+</style>
