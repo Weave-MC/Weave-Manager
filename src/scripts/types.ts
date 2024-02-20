63aad96486579f544fcf2b40504781a80e68c689
exejar <56853437+exejar@users.noreply.github.com>
2024-02-20 18:36:53 -0500

    Created Mod Profiles, Mod, and Agent types

--- src/scripts/types.ts
+++ src/scripts/types.ts
@@ -1,21 +1,65 @@
 export class LaunchProfile {
     public name: string
     public mcInfo: MinecraftInfo
+    public modProfile: ModProfile
 
     constructor(
         name: string,
-        mcInfo: MinecraftInfo
+        mcInfo: MinecraftInfo,
+        modProfile: ModProfile
     ) {
         this.name = name
         this.mcInfo = mcInfo
+        this.modProfile = modProfile
+
+        this.saveProfile()
     }
 
+    public saveProfile() {
+        if (this.modProfile)
+            this.modProfile.saveProfile()
+    }
     public launchProfile(): void {
 
     }
     public deleteProfile() {
 
     }
+}
+
+export class ModProfile {
+    public name: string
+    public mods: Mod[]
+
+    constructor(
+        name: string,
+        mods: Mod[]
+    ) {
+        this.name = name
+        this.mods = mods
+
+        this.saveProfile()
+    }
+
+    public saveProfile() {
+
+    }
+}
+
+export interface Mod {
+    name: string
+    description: string
+    version: string
+    authors: string[]
+    filePath: string
+    fileName: string
+    disabled: boolean
+}
+
+export interface Agent {
+    filePath: string
+    fileName: string
+    disabled: boolean
 }
 
 export interface MinecraftProcess {
