28bc83df1b27d5f74a713f28efc0ddc15b3af5f9
exejar <56853437+exejar@users.noreply.github.com>
2024-03-13 20:38:12 -0400

    Updated icon to svg and color it according to theme

--- src/components/HeaderBar.svelte
+++ src/components/HeaderBar.svelte
@@ -1,6 +1,5 @@
 <script lang="ts">
     import { appWindow } from '@tauri-apps/api/window'
-    import weaveIcon from '../weave-icon.png'
 
     function minimize() {
         appWindow.minimize()
@@ -21,7 +20,9 @@
         </button>
     </div>
     <div id="weave-icon-wrapper" class="w-14 h-14 flex justify-center items-center">
-        <img src="{weaveIcon}" id="weave-icon" class="w-11 h-11 m-auto" alt="Weave Icon">
+        <svg viewBox="0 0 300 300" class="fill-accent w-12 h-12 m-auto">
+            <path d="M213.359 57.92c7.916-11.875 25.366-11.875 33.282 0l33.991 50.986a20.002 20.002 0 0 1 0 22.188L206.641 242.08c-7.916 11.875-25.366 11.875-33.282 0L150 207.042l-.007.01-18.972-28.484-.002.002L70 87.042 48.028 120 110 212.958l7.519-11.278 9.682-14.524 18.02 27.055-2.74 4.11-15.84 23.759c-7.916 11.875-25.366 11.875-33.282 0l-73.99-110.986a20 20 0 0 1 0-22.188l33.99-50.986c7.916-11.875 25.366-11.875 33.282 0L150 152.958l.013-.019 18.019 27.054-.004.007L190 212.958 251.972 120 230 87.042l-27.519 41.279-29.688 44.532-18.02-27.055 22.746-34.118 35.84-53.76Z" clip-rule="evenodd"/>
+        </svg>
     </div>
     <div id="title" class="relative left-5 flex flex-row gap-1.5 items-baseline font-normal">
         <h1 class="text-2xl">Weave Manager</h1>
@@ -33,7 +34,6 @@
     #header-bar {
         z-index: 1;
     }
-
     .button {
         @apply h-8 aspect-square text-xl transition-colors duration-300 rounded;
         -webkit-app-region: no-drag;
